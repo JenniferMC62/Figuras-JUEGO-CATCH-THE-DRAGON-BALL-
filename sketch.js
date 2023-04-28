1b4dda6aa3f3076289eafa55fb9220e6ebb9a703
Jennifer Macedo <126447294+JenniferMC62@users.noreply.github.com>
2023-04-28 04:12:04 +0000

    juego actualizado

--- sketch.js
+++ sketch.js
@@ -1,201 +1,200 @@
-// Declaraciones 
-
-let balls = []; // Declara una matriz vacía llamada "balls"
-let ballTimer = 0; // Inicializa una variable "ballTimer" con el valor 0
-let score = 0; // Inicializa una variable "score" con el valor 0
-let level = 0; // Inicializa una variable "level" con el valor 0
-let dragonColor = "green"; // Inicializa una variable "dragonColor" con el valor "green"
-let winnerButton; // Declara una variable "winnerButton" sin asignarle un valor
-let scorePosition; // Declara una variable "scorePosition" sin asignarle un valor
-let levelPosition; // Declara una variable "levelPosition" sin asignarle un valor
+let movingObjects = []; // Creamos un array vacío llamado "movingObjects"
+let ballTimer = 0; // Inicializamos la variable "ballTimer" en 0
+let score = 0; // Inicializamos la variable "score" en 0
+let level = 0; // Inicializamos la variable "level" en 0
+let dragonColor = "green"; // Inicializamos la variable "dragonColor" en "green"
+let winnerButton; // Declaramos la variable "winnerButton" sin inicializarla
+let scorePosition; // Declaramos la variable "scorePosition" sin inicializarla
+let levelPosition; // Declaramos la variable "levelPosition" sin inicializarla
 
 
-// SETUP
 
-function setup() { // Inicio de la función "setup"
-  createCanvas(400, 400); // Crea un lienzo de dibujo de 400 x 400
-  dragon = new Dragon(); // Crea una nueva instancia de la clase "Dragon" y la asigna a la variable "dragon"
-  scorePosition = createVector(10, 30); // Crea un vector de posición (x, y) y lo asigna a la variable "scorePosition"
-  levelPosition = createVector(width - 10, 30); // Crea un vector de posición (x, y) y lo asigna a la variable "levelPosition"
+
+// Definimos la función "setup"
+function setup() {
+
+  createCanvas(400, 400);   // Creamos un canvas de 400x400 píxeles
+  movingObjects.push(new Dragon());   // Agregamos un nuevo objeto de tipo Dragon al array "movingObjects"
+  scorePosition = createVector(10, 30);  // Creamos un vector con coordenadas (10, 30) y lo asignamos a la variable "scorePosition"
+  levelPosition = createVector(width - 10, 30); // Creamos un vector con coordenadas (ancho - 10, 30) y lo asignamos a la variable "levelPosition"
 }
 
 
-// FUNCIÓN DRAW
 
+
+// Definimos la función "draw"
 function draw() {
-  background(220); // establece el color de fondo del canvas
-  for (let ball of balls) { // recorre el arreglo de bolas
-    ball.show(); // llama al método show de cada bola para dibujarla en el canvas
-    ball.move(); // llama al método move de cada bola para moverla en el canvas
-    if (ball.isOffscreen()) { // verifica si la bola ha salido del canvas
-      balls.splice(balls.indexOf(ball), 1); // elimina la bola del arreglo
+  // Establecemos el color de fondo en gris claro
+  background(220);
+
+  // Iteramos a través de cada objeto en el array "movingObjects"
+  for (let object of movingObjects) {
+    // Mostramos el objeto en la pantalla
+    object.show();
+    // Movemos el objeto
+    object.move();
+
+    // Si el objeto es una pelota y está fuera de la pantalla, lo eliminamos del array "movingObjects"
+    if (object instanceof Ball && object.isOffscreen()) {
+      movingObjects.splice(movingObjects.indexOf(object), 1);
     }
-    if (dragon.hits(ball)) { // verifica si el dragón ha chocado con la bola
-      score += ball.value; // aumenta el puntaje del jugador con el valor de la bola
-      if (score >= 1000 && level === 0) { // si el puntaje es mayor o igual a 1000 y el nivel es 0
-        level = 1; // sube al nivel 1
-        dragonColor = "blue"; // cambia el color del dragón a azul
-      } else if (score >= 2500 && level === 1) { // si el puntaje es mayor o igual a 2500 y el nivel es 1
-        level = 2; // sube al nivel 2
-        dragonColor = "purple"; // cambia el color del dragón a morado
-      } else if (score >= 5000 && level === 2) { // si el puntaje es mayor o igual a 5000 y el nivel es 2
-        level = 3; // sube al nivel 3
-        dragonColor = "red"; // cambia el color del dragón a rojo
-        noLoop(); // detiene la animación del juego
-        background(220); // establece el color de fondo del canvas
-        textSize(50); // establece el tamaño de fuente del texto
-        textAlign(CENTER, CENTER); // establece la alineación del texto
-        fill("black"); // establece el color de fuente del texto
-        text("WINNER", width / 2, height / 2); // dibuja el texto en el centro del canvas
-        winnerButton = createButton('Play Again'); // crea un botón para jugar de nuevo
-        winnerButton.position(width/2 - 50, height/2 + 40); // establece la posición del botón
-        winnerButton.mousePressed(playAgain); // llama a la función playAgain cuando se hace clic en el botón
-      }// Elimina la bola actual del arreglo de bolas
-balls.splice(balls.indexOf(ball), 1);
+
+    // Si el objeto es una pelota y la primera pelota en el array "movingObjects" colisiona con ella,
+    // actualizamos la puntuación y comprobamos si se cumple alguna condición de nivel
+    if (object instanceof Ball && movingObjects[0].hits(object)) {
+      score += object.value; // Incrementamos la puntuación según el valor de la pelota
+      // Comprobamos si se cumple alguna condición de nivel y actualizamos la variable "level" y "dragonColor" en consecuencia
+      if (score >= 1000 && level === 0) {
+        level = 1;
+        dragonColor = "blue";
+      } else if (score >= 2500 && level === 1) {
+        level = 2;
+        dragonColor = "purple";
+      } else if (score >= 5000 && level === 2) {
+        level = 3;
+        dragonColor = "red";
+        // Si el jugador ha llegado al nivel 3 y ha obtenido al menos 5000 puntos, se detiene el juego y se muestra un mensaje de ganador
+        noLoop();
+        background(220);
+        textSize(50);
+        textAlign(CENTER, CENTER);
+        fill("black");
+        text("WINNER", width / 2, height / 2);
+        // Creamos un botón para volver a jugar y lo mostramos en la pantalla
+        winnerButton = createButton('Play Again');
+        winnerButton.position(width/2 - 50, height/2 + 40);
+        winnerButton.mousePressed(playAgain);
+      }
+      // Eliminamos la pelota del array "movingObjects"
+      movingObjects.splice(movingObjects.indexOf(object), 1);
     }
-}
-// Muestra al dragón en la pantalla
-dragon.show();
+  }
 
-// Mueve al dragón en la pantalla de acuerdo a su posición actual
-dragon.move();
+  // Mostramos la puntuación y el nivel en la pantalla
+  fill(0);
+  textSize(20);
+  textAlign(LEFT, TOP);
+  text("Score: " + score, scorePosition.x, scorePosition.y);
+  textAlign(RIGHT, TOP);
+  text("Level: "+ level, levelPosition.x, levelPosition.y);
 
-// Cambia el color del dragón a dragonColor
-dragon.color = dragonColor;
-
-// Establece el color de relleno para el texto que se mostrará en la pantalla
-fill(0);
-
-// Establece el tamaño del texto que se mostrará en la pantalla
-textSize(20);
-
-// Establece la alineación horizontal y vertical del texto que se mostrará en la pantalla
-textAlign(LEFT, TOP);
-
-// Muestra el puntaje actual del jugador en la pantalla
-text("Score: " + score, scorePosition.x, scorePosition.y);
-
-// Establece la alineación horizontal y vertical del texto que se mostrará en la pantalla
-textAlign(RIGHT, TOP);
-
-// Muestra el nivel actual del jugador en la pantalla
-text("Level: "+ level, levelPosition.x, levelPosition.y);
-
-// Comprueba si ha pasado un segundo desde que se agregó la última bola, si el nivel actual es menor que 3 y el puntaje actual es menor que 5000
-if (millis() - ballTimer > 1000 && level < 3 && score < 5000) {
-  // Agrega una nueva bola al juego
-  balls.push(new Ball());
-
-  // Restablece el temporizador de bola (ballTimer) al tiempo actual
-  ballTimer = millis();
-}
+  // Si el nivel es menor que 3, la puntuación es menor que 5000 y se cumple una probabilidad del 2%, agregamos una nueva pelota al array "movingObjects"
+  if (random() < 0.02 && level < 3 && score < 5000) {
+    movingObjects.push(new Ball());
+  }
 }
 
 
 
-// Función que se llama cuando el jugador desea jugar de nuevo
 
+// Esta función se llama cuando se hace clic en el botón "Play Again".
+// Reinicia las variables del juego y crea un nuevo objeto Dragon.
 function playAgain() {
-  // Remueve el botón de ganador de la pantalla
-  winnerButton.remove();
-
-  // Reinicia el arreglo de bolas
-  balls = [];
-
-  // Reinicia el puntaje del jugador
-  score = 0;
-
-  // Reinicia el nivel del jugador
-  level = 0;
-
-  // Reinicia el color del dragón a verde
-  dragonColor = "green";
-
-  // Continúa ejecutando el juego
-  loop();
+  winnerButton.remove(); // Elimina el botón "Play Again".
+  movingObjects = []; // Vacía el arreglo de objetos móviles.
+  score = 0; // Reinicia la puntuación a cero.
+  level = 0; // Reinicia el nivel a cero.
+  dragonColor = "green"; // Restablece el color del dragón a verde.
+  movingObjects.push(new Dragon()); // Crea un nuevo objeto Dragon y lo agrega al arreglo de objetos móviles.
+  loop(); // Reanuda el bucle de dibujo.
 }
 
 
 
-
-// Clase Dragon para representar al personaje del jugador
-
-class Dragon {
+// Esta clase es la clase base para todos los objetos móviles del juego.
+class MovingObject {
   constructor() {
-    this.x = width / 2; // Posición inicial del personaje en el centro de la pantalla horizontalmente
-    this.y = height / 2; // Posición inicial del personaje en el centro de la pantalla verticalmente
-    this.speed = 5; // Velocidad de movimiento del personaje
-    this.size = 30; // Tamaño del personaje
-    this.color = "green"; // Color del personaje
+    this.size = 0; // El tamaño del objeto.
+    this.color = ""; // El color del objeto.
+    this.x = 0; // La posición X del objeto.
+    this.y = 0; // La posición Y del objeto.
   }
 
-  // Método para dibujar el personaje en la pantalla
+  // Muestra el objeto en la pantalla.
   show() {
-    fill(this.color); // Establece el color de relleno al color del personaje
-    ellipse(this.x, this.y, this.size, this.size); // Dibuja el personaje como un círculo
+    fill(this.color); // Establece el color de relleno del objeto.
+    ellipse(this.x, this.y, this.size, this.size); // Dibuja una elipse en la posición del objeto con el tamaño especificado.
   }
 
-  // Método para mover al personaje en la pantalla
+  // Mueve el objeto.
   move() {
-    if (keyIsDown(LEFT_ARROW)) { // Si el jugador presiona la tecla de flecha izquierda
-      this.x -= this.speed; // Disminuye la posición horizontal del personaje
-    } 
-    if (keyIsDown(RIGHT_ARROW)) { // Si el jugador presiona la tecla de flecha derecha
-      this.x += this.speed; // Aumenta la posición horizontal del personaje
-    } 
-    if (keyIsDown(UP_ARROW)) { // Si el jugador presiona la tecla de flecha arriba
-      this.y -= this.speed; // Disminuye la posición vertical del personaje
-    } 
-    if (keyIsDown(DOWN_ARROW)) { // Si el jugador presiona la tecla de flecha abajo
-      this.y += this.speed; // Aumenta la posición vertical del personaje
-    }
-    // Limita la posición del personaje dentro de los límites de la pantalla
-    this.x = constrain(this.x, 0 + this.size / 2, width - this.size / 2);
-    this.y = constrain(this.y, 0 + this.size / 2, height - this.size / 2);
+    // Esta función se implementa en las subclases para proporcionar un comportamiento de movimiento específico para cada tipo de objeto móvil.
   }
 
-  // Método para determinar si el personaje ha sido golpeado por una bola
-  hits(ball) {
-    let d = dist(this.x, this.y, ball.x, ball.y); // Calcula la distancia entre el personaje y la bola
-    return d < this.size / 2 + ball.size / 2; // Retorna verdadero si la distancia es menor que la suma de los radios del personaje y la bola
+  // Verifica si este objeto colisiona con otro objeto.
+  hits(object) {
+    // Esta función se implementa en las subclases para proporcionar un comportamiento específico de colisión para cada tipo de objeto móvil.
   }
 }
 
-// Clase Ball para representar las bolas (3 niveles - 3 colores diferentes)
-class Ball {
+
+
+// Clase Dragon que hereda de la clase MovingObject
+class Dragon extends MovingObject {
+// Constructor de la clase Dragon
+constructor() {
+super(); // Llama al constructor de la clase padre
+this.x = width / 2;
+this.y = height / 2;
+this.speed = 5;
+this.size = 30;
+this.color = "green";
+}
+
+// Método para mover el dragón
+move() {
+if (keyIsDown(LEFT_ARROW)) { // Si se presiona la flecha izquierda
+this.x -= this.speed; // Disminuye la posición x del dragón
+}
+if (keyIsDown(RIGHT_ARROW)) { // Si se presiona la flecha derecha
+this.x += this.speed; // Aumenta la posición x del dragón
+}
+if (keyIsDown(UP_ARROW)) { // Si se presiona la flecha arriba
+this.y -= this.speed; // Disminuye la posición y del dragón
+}
+if (keyIsDown(DOWN_ARROW)) { // Si se presiona la flecha abajo
+this.y += this.speed; // Aumenta la posición y del dragón
+}
+// Restringe el movimiento del dragón a los límites de la pantalla
+this.x = constrain(this.x, 0 + this.size / 2, width);
+this.y = constrain(this.y, 0 + this.size / 2, height - this.size / 2);
+}
+
+// Método que verifica si el dragón colisiona con otro objeto
+hits(object) {
+let d = dist(this.x, this.y, object.x, object.y); // Calcula la distancia entre el dragón y el objeto
+return d < (this.size / 2 + object.size / 2); // Retorna true si la distancia es menor a la suma de los radios del dragón y el objeto
+}
+}
+
+
+
+// Define la clase Ball que hereda de MovingObject
+class Ball extends MovingObject {
   constructor() {
-    // Tamaño y color aleatorios
-    this.size = 20;
-    this.color = random(["yellow", "blue", "red"]);
-    // Posición inicial y velocidad aleatoria
-    this.x = width + this.size / 2;
+    // Llama al constructor de la clase padre
+    super();
+    // Define las propiedades de la pelota
+    this.x = width;
     this.y = random(height);
-    this.xSpeed = random(-5, -1);
-    this.ySpeed = random(-5, 5);
-    // Valor de puntos basado en el color
-    if (this.color === "blue") {
-      this.value = 100;
-    } else if (this.color === "red") {
-      this.value = 200;
-    } else if (this.color === "yellow") {
-      this.value = 500;
-    }
-  }
- 
-  show() {
-    // Dibujar la pelota en la pantalla
-    fill(this.color);
-    ellipse(this.x, this.y, this.size, this.size);
+    this.speed = random(2, 5);
+    this.size = random(10, 30);
+    this.color = color(random(255), random(255), random(255));
+    // Define el valor de la pelota en base a su tamaño
+    this.value = Math.round(map(this.size, 10, 30, 100, 500));
   }
 
+  // Define el método move de la pelota
   move() {
-    // Mover la pelota en cada frame
-    this.x += this.xSpeed;
-    this.y += this.ySpeed;
+    // Actualiza la posición de la pelota en el eje x
+    this.x -= this.speed;
+    // Mueve la pelota aleatoriamente en el eje y
+    this.y += random(-1, 1); 
   }
 
+  // Define el método isOffscreen de la pelota
   isOffscreen() {
-    // Verificar si la pelota está fuera de la pantalla
-    return this.x < -this.size / 2;
+    // Devuelve verdadero si la pelota está fuera de la pantalla
+    return (this.x < 0);
   }
 }
